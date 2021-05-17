475ad93e6725d08e8d3515ab0ebfd36cd233b33e
Zach Taylor <zachtylr04@gmail.com>
2021-05-16 19:58:51 -0500

    Initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,34 @@
+import type { MetaFunction, LinksFunction, LoaderFunction } from "remix";
+import { useRouteData } from "remix";
+
+import stylesUrl from "../styles/index.css";
+
+export let meta: MetaFunction = () => {
+  return {
+    title: "Home | Tweet on Time",
+    description: "Schedule your tweets.",
+  };
+};
+
+export let links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: "" }];
+};
+
+export let loader: LoaderFunction = async () => {
+  return { message: "this is awesome 😎" };
+};
+
+export default function Index() {
+  let data = useRouteData();
+
+  return (
+    <div style={{ textAlign: "center", padding: 20 }}>
+      <h2>Welcome to Remix!</h2>
+      <p>
+        <a href="https://remix.run/dashboard/docs">Check out the docs</a> to get
+        started.
+      </p>
+      <p>Message from the loader: {data.message}</p>
+    </div>
+  );
+}
